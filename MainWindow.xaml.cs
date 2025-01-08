1ee4205a5ebe162418a86f4740f56fdb675ddcfc
x0rtex <x0rtex420@gmail.com>
2025-01-08 13:28:26 +0000

    Created event and ticket objects for data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿// https://github.com/x0rtex/JanExam
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,9 +9,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-/// 
-/// https://github.com/x0rtex/JanExam
-/// 
 namespace JanExam
 {
     /// <summary>
@@ -21,6 +19,13 @@ namespace JanExam
         public MainWindow()
         {
             InitializeComponent();
+
+            Event event1 = new("Oasis Croke Park", new DateTime(2025, 06, 20), EventType.Music);
+            Event event2 = new("Electric Picnic", new DateTime(2025, 08, 20), EventType.Music);
+            Ticket ticket1 = new("Early Bird", 100m, 100);
+            Ticket ticket2 = new("Platinum", 150m, 100);
+            VipTicket vipTicket1 = new("Ticket and Hotel Package", 150m, 100m, "4* Hotel", 100);
+            VipTicket vipTicket2 = new("Weekend Ticket", 200m, 100m, "With Camping", 100);
         }
     }
 }
