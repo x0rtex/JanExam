c58dd96372f6d4f96c46886e6beba8607f91d2df
x0rtex <x0rtex420@gmail.com>
2025-01-08 14:44:49 +0000

    Tickets are now shown as unavailable if there are no tickets left.

--- Ticket.cs
+++ Ticket.cs
@@ -12,6 +12,12 @@ namespace JanExam
         public decimal Price { get; set; } = price;
         public int AvailableTickets { get; set; } = availableTickets;
 
-        public override string ToString() => $"{Name} - {Price:c} [AVAILABLE - {AvailableTickets}]";
+        public override string ToString()
+        {
+            if (AvailableTickets <= 0)
+                return $"{Name} - {Price:c} [UNAVAILABLE]";
+
+            return $"{Name} - {Price:c} [AVAILABLE - {AvailableTickets}]";
+        } 
     }
 }
